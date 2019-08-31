3dd795fb9e5e9d7e77f7610214160f3ac48b2c5f
kedarv <kedar54@gmail.com>
2019-08-31 13:06:06 -0700

    wrap in container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,23 +3,40 @@ import './App.css';
 import cardData from '../../data.json';
 import UnicornCard from '../UnicornCard/UnicornCard';
 import 'semantic-ui-css/semantic.min.css'
-import { Grid, Image, Segment } from 'semantic-ui-react'
+import { Grid, Container } from 'semantic-ui-react'
+import Fuse from "fuse.js"
 
-function App() {
-  return (
-    <div className="App">
-      <form>
-        <div class="search-box">
-          <div class="search-box_wrapper">
-            <input class="card-input"></input>
-          </div>
-        </div>
-      </form>
-      <Grid stackable columns={4}>
-        {cardData.map((object, i) => <Grid.Column><UnicornCard {...object} key={i}></UnicornCard></Grid.Column>)}
-      </Grid>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: cardData,
+    }
+    let options = {
+      keys: ['name'],
+    };
+    this.fuse = new Fuse(cardData, options)
+  }
+
+  handleSearchChange = (e) => {
+    this.setState({items: this.fuse.search(e.target.value)});
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Container> 
+          <input
+              onChange={this.handleSearchChange}
+            />
+            <br/><br/>
+          <Grid stackable columns={4}>
+            {this.state.items.map((object, i) => <Grid.Column><UnicornCard {...object} key={i}></UnicornCard></Grid.Column>)}
+          </Grid>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;

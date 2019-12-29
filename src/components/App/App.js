06356f2fe0f1104731f3252f3735ce5dece93413
kedarv <kedar54@gmail.com>
2019-12-29 14:14:27 -0800

    fix search bar to be styled

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,37 +1,45 @@
-import React from 'react';
-import './App.css';
-import cardData from '../../data.json';
-import UnicornCard from '../UnicornCard/UnicornCard';
-import 'semantic-ui-css/semantic.min.css'
-import { Grid, Container } from 'semantic-ui-react'
-import Fuse from "fuse.js"
+import React from "react";
+import "./App.css";
+import cardData from "../../data.json";
+import UnicornCard from "../UnicornCard/UnicornCard";
+import "semantic-ui-css/semantic.min.css";
+import { Grid, Container, Input } from "semantic-ui-react";
+import Fuse from "fuse.js";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: cardData,
-    }
-    let options = {
-      keys: ['name'],
+      items: cardData
     };
-    this.fuse = new Fuse(cardData, options)
+    let options = {
+      keys: ["name"]
+    };
+    this.fuse = new Fuse(cardData, options);
   }
 
-  handleSearchChange = (e) => {
-    this.setState({items: this.fuse.search(e.target.value)});
-  }
-  
+  handleSearchChange = e => {
+    this.setState({ items: this.fuse.search(e.target.value) });
+  };
+
   render() {
     return (
       <div className="App">
-        <Container> 
-          <input
-              onChange={this.handleSearchChange}
-            />
-            <br/><br/>
+        <Container>
+          <Input
+            fluid
+            icon="search"
+            onChange={this.handleSearchChange}
+            placeholder="Search..."
+          />
+          <br />
+          <br />
           <Grid stackable columns={4}>
-            {this.state.items.map((object, i) => <Grid.Column><UnicornCard {...object} key={i}></UnicornCard></Grid.Column>)}
+            {this.state.items.map((object, i) => (
+              <Grid.Column>
+                <UnicornCard {...object} key={i}></UnicornCard>
+              </Grid.Column>
+            ))}
           </Grid>
         </Container>
       </div>
--- src/components/UnicornCard/UnicornCard.js
+++ src/components/UnicornCard/UnicornCard.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
-import { LazyLoadComponent } from 'react-lazy-load-image-component';
+import React from "react";
+import { Card, Image } from "semantic-ui-react";
+import { LazyLoadComponent } from "react-lazy-load-image-component";
 
-const UnicornCard = (props) => (
+const UnicornCard = props => (
   <Card>
-    <LazyLoadComponent><Image src={'../../card_images/' + props.img} wrapped ui={false} /></LazyLoadComponent>
+    <LazyLoadComponent>
+      <Image src={"../../card_images/" + props.img} wrapped ui={false} />
+    </LazyLoadComponent>
     <Card.Content>
       <Card.Header>{props.name}</Card.Header>
-      <Card.Meta>{props.type} / {props.deck} / {props.quantity}</Card.Meta>
+      <Card.Meta>
+        {props.type} / {props.deck} / {props.quantity}
+      </Card.Meta>
       <Card.Description>{props.text}</Card.Description>
     </Card.Content>
   </Card>
-)
+);
 
-export default UnicornCard
+export default UnicornCard;
